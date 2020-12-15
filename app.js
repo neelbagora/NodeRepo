1ea9a4afb3a01e466bb7bf80776cba935d9d03c2
neelbagora <neelbagora@gmail.com>
2020-12-14 23:28:48 -0800

    Fix PORT env issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   console.log('GET ', '/')
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("WAZZZAP");
 });
