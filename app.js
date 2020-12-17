0906593d77e59a45bb2a08da9ebb3327af5c0f84
neelbagora <neelbagora@gmail.com>
2020-12-16 17:39:23 -0800

    Added DB connection test.

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
 
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+let test = '';
+
+client.connect(err => {
+  if (err) {
+    test = 'connection error';
+  } else {
+    test = 'connected'
+  }
+})
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the home page baby');
+  res.send(`${test}`);
   console.log('GET ', '/')
 });
 
